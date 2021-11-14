510dcdbd5de7582607f5154219e27e541b16e1eb
mrossant <matteorossant@gmail.com>
2021-11-14 11:55:51 -0500

    create index route for logs

--- routes/api/logs.js
+++ routes/api/logs.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-router.get("/test", (req, res) => res.json({ msg: "This is the logs route" }));
+const Log = require('../../models/Log');
+
+router.get('/', (req, res) => {
+    Log.find()
+        .sort({ date: -1 })
+        .then(logs => res.json(logs))
+        .catch(err => res.status(404).json({nologsfound: 'No logs found'}));
+});
 
 module.exports = router;
