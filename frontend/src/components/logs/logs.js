2b5062861a512fa9ac6349bb397bb544a8154359
neil <neil.pandya.np@gmail.com>
2021-11-16 18:06:12 -0500

    about to use plotly.

--- frontend/src/components/logs/logs.js
+++ frontend/src/components/logs/logs.js
@@ -16,6 +16,7 @@ class Logs extends React.Component {
     }
 
     formatData(countHash) {
+        debugger
         const data = [];
         const uniqueDates = [];
         for (let i = 0; i < this.props.logs.data.length; i++) {
