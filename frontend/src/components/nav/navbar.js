35b9ff245ecc07d750f02f8a03fade3c2312314e
JortzFromSchool <johnnydortiz@gmail.com>
2021-11-16 14:14:31 -0500

    Added link to User Show Page from Nav Bar

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -18,6 +18,7 @@ class NavBar extends React.Component {
       if (this.props.loggedIn) {
         return (
             <div>
+                <Link to={`/users/${this.props.userId}`}>My Stats</Link>
                 <button onClick={this.logoutUser}>Logout</button>
             </div>
         );
