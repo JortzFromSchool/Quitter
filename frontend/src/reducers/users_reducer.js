8fbda286e3257a5ba3f0fa3906029a0e99eabf9e
JortzFromSchool <johnnydortiz@gmail.com>
2021-11-17 12:31:04 -0500

    users slice updated for all and admin subslices

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -1,16 +1,19 @@
-import { RECEIVE_USER } from "../actions/user_actions";
+import { RECEIVE_ADMIN, RECEIVE_USER } from "../actions/user_actions";
 
-const UsersReducer = (state = {}, action) => {
+const UsersReducer = (state = {all: {}, admin: {}}, action) => {
 Object.freeze(state);
 let newState = Object.assign({}, state);
 
 switch (action.type) {
   case RECEIVE_USER:
-    // console.log(action.user)
-    return {
-      ...state, [action.user.data._id]: action.user.data
-    }
-
+    const newUser = {[action.user.data._id]: action.user.data};
+    let newAll = Object.assign({}, state.all, newUser);
+    newState.all = newAll;
+    return newState;
+  case RECEIVE_ADMIN:
+    const newAdmin = {[action.admin.data._id]: action.admin.data}
+    newState.admin = newAdmin;
+    return newState;
   default:
     return state;
 }
