85d5b135acd0c5cb3238bacda5c9b9f54e884fbf
mrossant <matteorossant@gmail.com>
2021-11-18 13:44:46 -0500

    styling create log modal

--- frontend/src/components/logs/log_show.js
+++ frontend/src/components/logs/log_show.js
@@ -5,13 +5,13 @@ import Moment from 'react-moment';
 class LogShow extends React.Component {
     render() {
         let logTimeVariable = new Date(this.props.logTime);
-        let logTimeVariablePlusFive = new Date(logTimeVariable.getTime() + 5*60*60*1000)
+        // let logTimeVariablePlusFive = new Date(logTimeVariable.getTime() + 5*60*60*1000)
     
         return (
             <div className="log-show">
                 {this.props.description ? <div><strong>Description:</strong> &nbsp; {this.props.description}</div> : null}
-                <div><strong>Date:</strong> &nbsp; {logTimeVariablePlusFive.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: '2-digit' })}</div>
-                <div><strong>Time:</strong> &nbsp; {logTimeVariablePlusFive.toLocaleString('en-US', { hour: 'numeric', hour12: true })}</div>
+                <div><strong>Date:</strong> &nbsp; {logTimeVariable.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: '2-digit' })}</div>
+                <div><strong>Time:</strong> &nbsp; {logTimeVariable.toLocaleString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' })}</div>
             </div>
         );
     };
