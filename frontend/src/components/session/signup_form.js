1f715d91a0ec08127090195054919b71b4d68922
neil <neil.pandya.np@gmail.com>
2021-12-06 13:49:26 -0500

    Render sign up errors.

--- frontend/src/components/session/signup_form.js
+++ frontend/src/components/session/signup_form.js
@@ -14,17 +14,18 @@ class SignupForm extends React.Component {
     };
     this.handleDemo = this.handleDemo.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.clearedErrors = false;
+    this.renderErrors = this.renderErrors.bind(this);
+    // this.clearedErrors = false;
   }
 
-  // componentWillReceiveProps(nextProps) {
+  componentWillReceiveProps(nextProps) {
 
-  //   if (this.props.currentUser) {
-  //     this.props.history.push(`/users/${this.props.currentUser.id}`);
-  //   }
+    // if (this.props.currentUser) {
+    //   this.props.history.push(`/users/${this.props.currentUser.id}`);
+    // }
 
-  //   this.setState({errors: nextProps.errors})
-  // }
+    this.setState({errors: nextProps.errors})
+  }
 
   update(field) {
     return e => this.setState({
@@ -53,12 +54,16 @@ class SignupForm extends React.Component {
       password: this.state.password,
       password2: this.state.password2
     };
-
+    
     this.props.signup(user)
-    .then(() => this.props.login({
-      email: this.state.email,
-      password: this.state.password
-    }))
+    .then((res) => {
+      if (res.type === 'RECEIVE_USER_SIGN_IN') {
+        this.props.login({
+          email: user.email,
+          password: user.password
+        })
+      }
+    });
   }
 
   renderErrors() {
